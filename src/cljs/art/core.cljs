85566c9ff530c48d50f1b2210ff084b8555d630f
lsdafjklsd <ryanhaywoodj@gmail.com>
2016-12-01 00:08:27 -0500

    create parser / reconciler / send function, fulfill artworks send with data, render basic app

--- src/cljs/art/core.cljs
+++ src/cljs/art/core.cljs
@@ -13,7 +13,7 @@
 
 (defn request [method url cb]
   (let [headers {"Accept" "application/json"
-                 "X-Access-Token" ""
+                 "X-Access-Token" "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ODNmNjY0YWIyMDJhMzY0NjMwMDA3YjciLCJzYWx0X2hhc2giOiI1ZmY2MDUwMmEwZGIxNjZiMGMyZTRkNTkzODUxOGQxYyIsInJvbGVzIjoidXNlciIsInBhcnRuZXJfaWRzIjpbXSwiZXhwIjoyMjY5NDY4MzczLCJpYXQiOjE0ODA1NDk5NzMsImF1ZCI6IjUzZmYxYmNjNzc2ZjcyNDBkOTAwMDAwMCIsImlzcyI6IkdyYXZpdHkiLCJqdGkiOiI1ODNmNjY1NWNkNTMwZTY0NTcwMDA3ODMifQ.g6LlGHgDfs9tgusrNxteeP6LzUcZmy0hDoARUU2jiu0"
                  "Content-Type" "application/json"}
         handler (fn [res] (cb (keywordize-keys res)))]
     (method url {:handler handler :headers headers})))
@@ -32,7 +32,7 @@
                        :_embedded
                        :artists
                        (get 0)
-                       (select-keys [:name :loctation :id]))
+                       (select-keys [:name :location :id]))
             artwork-props (select-keys artwork [:id :title :category])]
         (put! out-c (merge artwork-props {:image image-link
                                           :artist artist}))))
@@ -47,23 +47,6 @@
           chan-array (map get-artwork-details artworks)]
       {:artworks (<! (async/map vector chan-array))})))
 
-(defui App
-  Object
-  (render [this]
-    (let [artworks (:artworks (om/props this))
-          _ (pprint (om/props this))]
-      (apply dom/div nil
-        (map (fn [{:keys [image artist] :as ff}]
-               (dom/img #js {:src image}))
-          artworks)))))
-
-(def app (om/factory App))
-
-(go
-  (let [artworks (<! (get-artworks))]
-    (reset! dbg artworks)
-    (js/ReactDOM.render (app artworks) (gdom/getElement "app"))))
-
 (defui Artist
   static om/Ident
   (ident [this {:keys [id]}]
@@ -71,7 +54,16 @@
 
   static om/IQuery
   (query [this]
-    [:name :loctation :id]))
+    [:name :location :id])
+
+  Object
+  (render [this]
+    (let [{:keys [name location]} (om/props this)]
+      (dom/div nil
+        (dom/h1 nil (str "By: " name))
+        (dom/h2 nil (str "Location: " location))))))
+
+(def artistt (om/factory Artist))
 
 (defui Artwork
   static om/Ident
@@ -80,12 +72,54 @@
 
   static om/IQuery
   (query [this]
-    [:id :title :category :image {:artist (om/get-query Artist)}]))
+    [:id :title :category :image {:artist (om/get-query Artist)}])
+
+  Object
+  (render [this]
+    (let [{:keys [title category image artist]} (om/props this)]
+      (dom/div nil
+        (dom/h1 nil title)
+        (dom/h2 nil category)
+        (dom/img #js {:src image})
+        (artistt artist)))))
+
+(def artwork (om/factory Artwork))
 
 (defui Artworks
   static om/IQuery
   (query [this]
-    [{:artworks (om/get-query Artwork)}]))
+    [{:artworks (om/get-query Artwork)}])
 
-(comment
-  (pprint (om/tree->db (om/get-query Artworks) @dbg true)))
+  Object
+  (render [this]
+    (let [artworks (:artworks (om/props this))]
+      (apply dom/div nil
+        (map #(dom/div nil
+                (artwork %)
+                (dom/hr nil)) artworks)))))
+
+(defmulti readd om/dispatch)
+
+(defmethod readd :artworks
+  [{:keys [state query] :as fff} key _]
+  (let [st @state]
+    (if-let [val (get st key)]
+      {:value (om/db->tree query val st)}
+      {:artworks true})))
+
+(defmethod readd :default
+  [{:keys [state]}])
+
+(defn sendd [{:keys [artworks]} cb]
+  (when artworks
+    (go
+      (let [_artworks (<! (get-artworks))]
+        (cb (om/tree->db artworks _artworks true))))))
+
+(def p (om/parser {:read #'readd}))
+(def r (om/reconciler {:state (atom nil)
+                       :parser p
+                       :send #'sendd
+                       :remotes [:artworks]}))
+
+(om/add-root! r Artworks (gdom/getElement "app"))
